25accda05b7f4e6895fd67a7868410d001d338c0
Kostiantyn Kobetskyi <kostiantyn_kobetskyi@epam.com>
2024-03-03 23:46:11 +0200

    updated scopes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,12 +3,11 @@ export const environment = {
     msalConfig: {
         auth: {
             clientId: '69111799-c2ca-490f-929f-4e5ee63b9792',
-            authority: 'https://login.microsoftonline.com/common/oauth2/authorize?resource=https://globaldisco.crm.dynamics.com/api/discovery/v2.0/Instances'
+            authority: 'https://login.microsoftonline.com/common/oauth2/authorize?resource=https://globaldisco.crm.dynamics.com'
         }
     },
     apiConfig: {
-        scopes: ['https://admin.services.crm.dynamics.com/user_impersonation'],
+        scopes: ['https://globaldisco.crm.dynamics.com/user_impersonation'],
         uri: 'https://globaldisco.crm.dynamics.com/api/discovery/v2.0/Instances'
     }
   };
-  
