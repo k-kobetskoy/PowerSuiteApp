0be60fccddbd0911d0ceec8d0e40357d2679b3f6
Kostiantyn Kobetskyi <kostiantyn_kobetskyi@epam.com>
2024-03-03 17:06:38 +0200

    initial

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: false,
+    msalConfig: {
+        auth: {
+            clientId: '69111799-c2ca-490f-929f-4e5ee63b9792',
+            authority: 'https://login.microsoftonline.com/common/oauth2/authorize?resource=https://globaldisco.crm.dynamics.com/api/discovery/v2.0/Instances'
+        }
+    },
+    apiConfig: {
+        scopes: ['https://admin.services.crm.dynamics.com/user_impersonation'],
+        uri: 'https://globaldisco.crm.dynamics.com/api/discovery/v2.0/Instances'
+    }
+  };
+  
