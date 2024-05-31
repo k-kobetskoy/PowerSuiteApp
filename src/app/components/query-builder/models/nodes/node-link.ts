e160fd1b3c0cfd536d9b11ccde6972ca6a0fab14
Kostiantyn Kobetskyi <kostiantyn_kobetskyi@epam.com>
2024-05-31 03:35:32 +0300

    Refactor entity form component to remove unused imports and optimize code

--- src/app/components/query-builder/models/nodes/node-link.ts
+++ src/app/components/query-builder/models/nodes/node-link.ts
@@ -1,4 +1,3 @@
-import { ITagProperties } from "../abstract/i-tag-properties";
 import { QueryNodeActions } from "../constants/query-node-actions";
 import { QueryNodeOrder } from "../constants/query-node-order.enum";
 import { QueryNodeType } from "../constants/query-node-type";
